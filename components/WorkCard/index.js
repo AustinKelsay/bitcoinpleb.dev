f83353f0230ac1db2b904fdd90b1927023bbffc2
austinkelsay <austinkelsay@yahoo.com>
2024-03-07 17:01:11 -0600

    Add project, add github icon

--- components/WorkCard/index.js
+++ components/WorkCard/index.js
@@ -1,5 +1,12 @@
 import React, { useState, useEffect } from "react";
 
+const GithubIcon = ({ size = 24 }) => (
+  <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-brand-github" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+    <path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5" />
+  </svg>
+);
+
+
 const WorkCard = ({ img, name, role, description, onClick }) => {
   const [backgroundSize, setBackgroundSize] = useState("cover");
   const [backgroundHeight, setBackgroundHeight] = useState("400px");
@@ -45,9 +52,14 @@ const WorkCard = ({ img, name, role, description, onClick }) => {
         }}
       >
       </div>
-      <h1 className="mt-5 text-3xl font-medium">
-        {name ? name : "Project Name"}
-      </h1>
+      <div className="flex flex-row w-full justify-between">
+        <h1 className="mt-5 text-3xl font-medium">
+          {name ? name : "Project Name"}
+        </h1>
+        <div className="mt-5 hover:opacity-60 cursor-pointer">
+          <GithubIcon size={44} />
+        </div>
+      </div>
       <h2 className="text-xl opacity-50">
         {role ? role : "Role"}
       </h2>
