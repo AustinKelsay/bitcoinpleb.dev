d3b7c9a60af0500fa41604911858541776224261
austinkelsay <austinkelsay@yahoo.com>
2024-07-01 12:38:19 -0500

    Fix maxSendable

--- pages/api/lnurlp/[slug].js
+++ pages/api/lnurlp/[slug].js
@@ -19,7 +19,7 @@ export default async function handler(req, res) {
 
         res.status(200).json({
             callback: `${BACKEND_URL}/api/callback/austin`,
-            maxSendable: 1000000,
+            maxSendable: 10000000000,
             minSendable: 1000,
             metadata: JSON.stringify(metadata),
             tag: 'payRequest'
