01ab2eeb4ae3339db6253c0a4acd485c4b8390d7
austinkelsay <austinkelsay@yahoo.com>
2024-07-31 14:44:01 -0500

    Fix lnurlp and callback endpoints after mixup

--- pages/api/callback/[slug].js
+++ pages/api/callback/[slug].js
@@ -1,44 +1,65 @@
-import axios from "axios"
-import crypto from "crypto"
-import { runMiddleware, corsMiddleware } from "../../../utils/middleware"
+import axios from "axios";
+import crypto from "crypto";
+import { runMiddleware, corsMiddleware } from "../../../utils/middleware";
+import { verifyEvent } from 'nostr-tools/pure';
 
-const BACKEND_URL = process.env.BACKEND_URL
+const BACKEND_URL = process.env.BACKEND_URL;
+const NOSTR_PUBKEY = process.env.NOSTR_PUBKEY; // Your nostr public key
 
 export default async function handler(req, res) {
     await runMiddleware(req, res, corsMiddleware);
-
-    const { slug, ...queryParams } = req.query
+    const { slug, ...queryParams } = req.query;
 
     if (slug === 'austin') {
         if (queryParams.amount) {
-            const metadata = [
-                ["text/plain", "Austin's lnurlp endpoint, CHEERS!"]
-            ];
+            const amount = parseInt(queryParams.amount);
+            let metadata, metadataString, hash, descriptionHash;
 
-            const metadataString = JSON.stringify(metadata);
+            if (queryParams.nostr) {
+                // This is a zap request
+                const zapRequest = JSON.parse(decodeURIComponent(queryParams.nostr));
 
-            const hash = crypto.createHash('sha256').update(metadataString).digest('hex');
+                // Verify the zap request
+                if (!verifyEvent(zapRequest)) {
+                    res.status(400).json({ error: 'Invalid zap request' });
+                    return;
+                }
 
-            const descriptionHash = Buffer.from(hash, 'hex').toString('base64'); // Encoding as base64
+                // Validate zap request
+                if (zapRequest.kind !== 9734 || !zapRequest.tags.find(tag => tag[0] === 'p' && tag[1] === NOSTR_PUBKEY)) {
+                    res.status(400).json({ error: 'Invalid zap request' });
+                    return;
+                }
+
+                metadataString = JSON.stringify(zapRequest);
+                hash = crypto.createHash('sha256').update(metadataString).digest('hex');
+                descriptionHash = Buffer.from(hash, 'hex').toString('base64');
+            } else {
+                // This is a regular lnurl-pay request
+                metadata = [
+                    ["text/plain", "Austin's lnurlp endpoint, CHEERS!"]
+                ];
+                metadataString = JSON.stringify(metadata);
+                hash = crypto.createHash('sha256').update(metadataString).digest('hex');
+                descriptionHash = Buffer.from(hash, 'hex').toString('base64');
+            }
 
             // Convert amount from millisatoshis to satoshis
-            const value = parseInt(queryParams.amount) / 1000
-
+            const value = amount / 1000;
             if (value < 1) {
-                res.status(400).json({ error: 'Amount too low' })
-                return
+                res.status(400).json({ error: 'Amount too low' });
+                return;
             } else {
-                axios.post(`${BACKEND_URL}/api/lnd`, { amount: value, description_hash: descriptionHash })
-                    .then(function (response) {
-                        res.status(200).json({ pr: response.data })
-                    })
-                    .catch(function (error) {
-                        console.log(error);
-                    });
+                try {
+                    const response = await axios.post(`${BACKEND_URL}/api/lnd`, { amount: value, description_hash: descriptionHash });
+                    res.status(200).json({ pr: response.data });
+                } catch (error) {
+                    console.error(error);
+                    res.status(500).json({ error: 'Failed to generate invoice' });
+                }
             }
         } else {
-            res.status(400).json({ error: 'Amount not specified' })
-            return
+            res.status(400).json({ error: 'Amount not specified' });
         }
     }
 }
--- pages/api/lnurlp/[slug].js
+++ pages/api/lnurlp/[slug].js
@@ -1,65 +1,32 @@
-import axios from "axios";
-import crypto from "crypto";
-import { runMiddleware, corsMiddleware } from "../../../utils/middleware";
-import { verifyEvent } from 'nostr-tools/pure';
+import { runMiddleware, corsMiddleware } from "../../../utils/middleware"
 
-const BACKEND_URL = process.env.BACKEND_URL;
-const NOSTR_PUBKEY = process.env.NOSTR_PUBKEY; // Your nostr public key
+const BACKEND_URL = process.env.BACKEND_URL
+const NOSTR_PUBKEY = process.env.NOSTR_PUBKEY
 
 export default async function handler(req, res) {
     await runMiddleware(req, res, corsMiddleware);
-    const { slug, ...queryParams } = req.query;
+
+    const { slug } = req.query
+
+    if (!slug || slug === 'undefined') {
+        res.status(404).json({ error: 'Not found' })
+        return
+    }
 
     if (slug === 'austin') {
-        if (queryParams.amount) {
-            const amount = parseInt(queryParams.amount);
-            let metadata, metadataString, hash, descriptionHash;
+        const metadata = [
+            ["text/plain", "Austin's lnurlp endpoint, CHEERS!"]
+        ];
 
-            if (queryParams.nostr) {
-                // This is a zap request
-                const zapRequest = JSON.parse(decodeURIComponent(queryParams.nostr));
-
-                // Verify the zap request
-                if (!verifyEvent(zapRequest)) {
-                    res.status(400).json({ error: 'Invalid zap request' });
-                    return;
-                }
-
-                // Validate zap request
-                if (zapRequest.kind !== 9734 || !zapRequest.tags.find(tag => tag[0] === 'p' && tag[1] === NOSTR_PUBKEY)) {
-                    res.status(400).json({ error: 'Invalid zap request' });
-                    return;
-                }
-
-                metadataString = JSON.stringify(zapRequest);
-                hash = crypto.createHash('sha256').update(metadataString).digest('hex');
-                descriptionHash = Buffer.from(hash, 'hex').toString('base64');
-            } else {
-                // This is a regular lnurl-pay request
-                metadata = [
-                    ["text/plain", "Austin's lnurlp endpoint, CHEERS!"]
-                ];
-                metadataString = JSON.stringify(metadata);
-                hash = crypto.createHash('sha256').update(metadataString).digest('hex');
-                descriptionHash = Buffer.from(hash, 'hex').toString('base64');
-            }
-
-            // Convert amount from millisatoshis to satoshis
-            const value = amount / 1000;
-            if (value < 1) {
-                res.status(400).json({ error: 'Amount too low' });
-                return;
-            } else {
-                try {
-                    const response = await axios.post(`${BACKEND_URL}/api/lnd`, { amount: value, description_hash: descriptionHash });
-                    res.status(200).json({ pr: response.data });
-                } catch (error) {
-                    console.error(error);
-                    res.status(500).json({ error: 'Failed to generate invoice' });
-                }
-            }
-        } else {
-            res.status(400).json({ error: 'Amount not specified' });
-        }
+        res.status(200).json({
+            callback: `${BACKEND_URL}/api/callback/austin`,
+            maxSendable: 10000000000,
+            minSendable: 1000,
+            metadata: JSON.stringify(metadata),
+            tag: 'payRequest',
+            allowNostr: true,
+            nostrPubkey: NOSTR_PUBKEY
+        })
+        return
     }
 }
