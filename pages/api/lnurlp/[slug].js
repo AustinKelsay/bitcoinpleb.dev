99629784977c7ea711baa82c6d4de0ac28103f50
Austin Kelsay <53542748+AustinKelsay@users.noreply.github.com>
2024-09-29 15:50:52 -0500

    Update [slug].js

--- pages/api/lnurlp/[slug].js
+++ pages/api/lnurlp/[slug].js
@@ -4,7 +4,7 @@ const BACKEND_URL = process.env.BACKEND_URL
 const NOSTR_PUBKEY = process.env.NOSTR_PUBKEY
 
 export default async function handler(req, res) {
-    console.log("host", req.headers.host);
+    console.log("host", req);
     await runMiddleware(req, res, corsMiddleware);
 
     const { slug } = req.query
