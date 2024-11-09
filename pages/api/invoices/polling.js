240eac540180286056f147529ffaff7582f0e8a1
austinkelsay <austinkelsay@yahoo.com>
2024-11-08 18:24:48 -0600

    Remove unused vars

--- pages/api/invoices/polling.js
+++ pages/api/invoices/polling.js
@@ -41,7 +41,7 @@ export default async function handler(req, res) {
                 const invoiceData = await kv.get(key);
                 if (!invoiceData) continue;
 
-                const { name, foundAddress, zapRequest, settled } = invoiceData;
+                const { zapRequest, settled } = invoiceData;
                 const paymentHash = key.replace('invoice:', '');
 
                 // Skip if already settled
